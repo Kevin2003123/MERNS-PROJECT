8a888188e8892e591608c91c784d68ed7ee9aa47
Kevin Fangio Reyes Lopez <kevinreyes005@gmail.com>
2020-08-13 01:15:22 -0400

    Initial commit

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const connectDb = require('./config/db');
+
+//connect to the server
+connectDb();
 
 app.get('/', (req, res) => res.send('API Running'));
 const PORT = process.env.PORT || 5000;
