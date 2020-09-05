228a19866d3c94aa286403dc5856639cc0bf40b5
Kevin Fangio Reyes Lopez <kevinreyes005@gmail.com>
2020-09-04 20:32:47 -0400

    make it better

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3100;
 
 app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
